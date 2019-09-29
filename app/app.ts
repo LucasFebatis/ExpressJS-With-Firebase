a00be5e020d2e8aabf2f398bb0954065b28b753c
Lucas Batista <lfebatis@gmail.com>
2019-09-29 12:44:17 -0300

    Criando Heloo World em TS

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,7 @@
-var express = require('express');
-var app = express();
+import express = require('express');
+
+// Create a new express application instance
+const app: express.Application = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
